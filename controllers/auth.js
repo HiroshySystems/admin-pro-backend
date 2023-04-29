46d2a20dcf07556113a73a6bb60fa95ca95276f8
Hiroshy Rojas <hiroshysystems@gmail.com>
2023-04-29 17:27:10 -0500

    Seccion 12 - Google

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,6 +2,7 @@ const {response} = require('express');
 const Usuario = require('../models/usuario');
 const bcrypt = require('bcryptjs');
 const { generarJWT } = require('../helpers/jwt');
+const { googleVerify } = require('../helpers/google-verify');
 
 const login = async(req,res = response) =>{
     const {email,password} = req.body;
@@ -42,6 +43,51 @@ const login = async(req,res = response) =>{
     }
 }
 
+const googleSigIn = async(req,res=response)=>{
+    const googleToken = req.body.token;
+    try {
+        const {name,email,picture} = await googleVerify(googleToken);
+
+        const usuarioDB = await Usuario.findOne({email});
+        let usuario;
+        if(!usuarioDB){
+            //si no existe el usuario
+            usuario = new Usuario({
+                nombre:name,
+                email,
+                password:'@@@',
+                img:picture,
+                google:true
+            })
+        }else{
+            //existe usuario
+            usuario = usuarioDB;
+            usuario.google = true;
+            usuario.password = '@@@';
+        }
+
+        //guardar en BD
+        await usuario.save();
+
+
+        //Generar el Token - JWT
+        const token = await generarJWT(usuario.id);
+
+        res.json({
+            ok:true,
+            msg:'Google SignIn',
+            token
+        })
+    } catch (error) {
+        res.status(401).json({
+            ok:false,
+            msg:'El Token no es correcto'
+        })
+    }   
+    
+}
+
 module.exports = {
-    login
+    login,
+    googleSigIn
 }
